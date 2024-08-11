2c3fe5b113c3ca6bbac142f84c754bf4402bad48
Aymansdk <ayman.sdk123@outlook.com>
2024-08-11 15:38:19 +0100

    refactor: Update landing page layout and content

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="relative bg-black-100">
       <div>
-        <h1>Hello Aymane !!</h1>
+        <h1>Hello Aymane!!</h1>
       </div>
     </main>
   );
